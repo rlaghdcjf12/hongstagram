7c5915b92ab693ef4f7062e4497492ac7fa07394
Hong <rlaghdcjf12@naver.com>
2020-12-13 21:57:24 +0900

    front_end : react, redux set-up completed

---
+++ front_end/src/store/configure.js
@@ -0,0 +1,11 @@
+import { createStore, applyMiddleware, compose } from "redux";
+import { createEpicMiddleware } from "redux-observable";
+import { rootReducers } from "./modules";
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const epicMiddleware = createEpicMiddleware();
+
+export default createStore(
+    rootReducers,
+    composeEnhancers(applyMiddleware(epicMiddleware))    
+);
